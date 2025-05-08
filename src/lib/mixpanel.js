2deaf0f7f32683bb00ab929a6d64f5e548a460f9
Priya <priya@sathyaswaroops-MacBook-Pro.local>
2025-05-08 18:07:57 +0530

    removed mixpanel integration

--- src/lib/mixpanel.js
+++ src/lib/mixpanel.js
@@ -1,12 +1,12 @@
-import mixpanel from "mixpanel-browser";
+// import mixpanel from "mixpanel-browser";
 
-const MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN;
-// import.meta.env.VITE_MIXPANEL_TOKEN;
+// const MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN;
+// // import.meta.env.VITE_MIXPANEL_TOKEN;
 
-mixpanel.init(MIXPANEL_TOKEN, {
-  debug: false,
-  track_pageview: true,
-  persistence: "localStorage",
-});
+// mixpanel.init(MIXPANEL_TOKEN, {
+//   debug: false,
+//   track_pageview: true,
+//   persistence: "localStorage",
+// });
 
-export default mixpanel;
+// export default mixpanel;
